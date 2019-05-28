52cdd9ae88671927cc0e75d6e34881d61ad320cd
gprodriguez2 <gprodriguez2@esei.uvigo.es>
2019-05-28 19:01:13 +0200

    + Added README and LICENSE files + Added some comments to the code

---
+++ README.md
@@ -0,0 +1,24 @@
+![picture alt](https://img.shields.io/badge/Java-1.8-blue.svg "Minimum Java version")
+![picture alt](https://img.shields.io/badge/build-passing-brightgreen.svg "Build passing")
+[![picture alt](https://img.shields.io/badge/license-unlicense-informational.svg "Unlicense")](https://github.com/abmiguez/dummyServer/blob/master/LICENSE.md)
+
+## Dummy Server in Java ##
+The code provided in this Github page is an Annotation Server prototype built in Java language. 
+
+The Dummy Server is designed to help researchers and developers to build a simply system in order to listen and respond BeCalm requests as quickly as possible, during the TIPS task in the BioCreative V.5 competition.
+
+### Glossary ###
+* [BioCreative V.5 competition](https://biocreative.bioinformatics.udel.edu/resources/publications/bcv5_proceedings "BioCreative V.5 competition")
+* [TIPS task and Annotation Servers](https://biocreative.bioinformatics.udel.edu/media/store/files/2017/BioCreative_V5_paper3.pdf "TIPS task and Annotation Servers")
+* [BeCalm Metaserver](http://www.becalm.eu/ "BeCalm metaserver")
+ 
+### Quick install ###
+Clone the repository and make a JAR or simply download the most recent release. Then, open a terminal and execute the following command to start the Server:
+
+`java [-Xmx2G] [-Xms256M] -server -jar dummyserver.jar`
+
+Note that, the parameters between brackets are optional. However, it may be interesting to modify the maximum memory allocation and the initial memory allocation pool when starting the JVM (-Xmx and -Xms, respectively).
+
+Once the Annotation Server is running, it will be possible to make requests! If you clone the code and open it in a IDE, you will see a JUnit test to make this requests automatically. To do this, you must execute the Server class first.
+### License ###
+Copyright © 2019, Aitor Blanco Míguez. Released under the Unlicense license.
--- src/test/java/es/uvigo/ei/sing/dummyserver/Requester.java
+++ src/test/java/es/uvigo/ei/sing/dummyserver/Requester.java
@@ -1,9 +1,6 @@
 package es.uvigo.ei.sing.dummyserver;
 
-import static es.uvigo.ei.sing.dummyserver.Constants.SERVERPORT;
-import static es.uvigo.ei.sing.dummyserver.Constants.REQUESTERPORT;
-import static es.uvigo.ei.sing.dummyserver.Constants.REQUEST_SECRET_KEY;
-import static java.util.concurrent.CompletableFuture.runAsync;
+import junit.framework.TestCase;
 
 import java.io.IOException;
 import java.io.ObjectInputStream;
@@ -14,101 +11,103 @@ import java.net.UnknownHostException;
 import java.util.Random;
 import java.util.concurrent.CompletableFuture;
 
-import junit.framework.TestCase;
+import static es.uvigo.ei.sing.dummyserver.Constants.*;
+import static java.util.concurrent.CompletableFuture.runAsync;
 
 public class Requester extends TestCase {
-	private ServerSocket serverSocket;
-	private Socket requestSocket;
-	private ObjectOutputStream out;
-	private ObjectInputStream in;
-	
-    public void testRequest(){    	
-    	boolean listening = true;
+    private ServerSocket serverSocket;
+    private Socket requestSocket;
+    private ObjectOutputStream out;
+    private ObjectInputStream in;
+
+    public void testRequest() {
+        boolean listening = true;
 
         try {
             serverSocket = new ServerSocket(REQUESTERPORT);
-	        System.out.println("client> Server started at port " + REQUESTERPORT);
-	        request().exceptionally(err -> {
-				System.out.println("Error while init socket: " + err);
-				return null;
-			});
-	        
-	        while (listening)
-	          new ClientThread(serverSocket.accept()).start();
-	        
-	        System.out.println("server> Server stoped");
-        
-			serverSocket.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-    	
+            System.out.println("client> Server started at port " + REQUESTERPORT);
+            request().exceptionally(err -> {
+                System.out.println("Error while init socket: " + err);
+                return null;
+            });
+
+            while (listening)
+                new ClientThread(serverSocket.accept()).start();
+
+            System.out.println("server> Server stoped");
+
+            serverSocket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
         assertTrue(true);
     }
-    
+
     private CompletableFuture<Void> request() throws IOException {
-    	return runAsync(() -> { 
-    		try{    		
-        		requestSocket = new Socket("localhost", SERVERPORT);
+        return runAsync(() -> {
+            try {
+                requestSocket = new Socket("localhost", SERVERPORT);
                 System.out.println("client> Connected to localhost in port " + SERVERPORT);
-                
+
                 out = new ObjectOutputStream(requestSocket.getOutputStream());
                 out.flush();
                 in = new ObjectInputStream(requestSocket.getInputStream());
-                
+
+                // Use a random value to request one of the two methods
                 final Random random = new Random();
                 String method = "getState";
-    		    if(random.nextBoolean())
-    		    	method = "getAnnotations";
-    		    final String petition = "{\"name\":\"BeCalm\", \"method\":\""+method+"\", \"becalm_key\":\""+REQUEST_SECRET_KEY+"\","+
-    		                            "\"custom_parameters\" :{\"example\":true}, \"parameters\" : {} }";
-    		    out.writeObject(petition);
+                if (random.nextBoolean())
+                    method = "getAnnotations";
+                final String petition = "{\"name\":\"BeCalm\", \"method\":\"" + method + "\", \"becalm_key\":\"" + REQUEST_SECRET_KEY + "\"," +
+                        "\"custom_parameters\" :{\"example\":true}, \"parameters\" : {} }";
+                out.writeObject(petition);
                 out.flush();
-                
+
                 System.out.println("client> " + petition);
-                
-                System.out.println("server> " + (String)in.readObject());
-                
-            }catch(UnknownHostException unknownHost){
+
+                System.out.println("server> " + (String) in.readObject());
+
+            } catch (UnknownHostException unknownHost) {
                 System.err.println("client> You are trying to connect to an unknown host!");
-            }catch(IOException ioException){
+            } catch (IOException ioException) {
                 ioException.printStackTrace();
-            }catch (ClassNotFoundException e) {
-    			e.printStackTrace();
-    		}finally{
-                try{
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            } finally {
+                try {
                     in.close();
                     out.close();
                     requestSocket.close();
-                }
-                catch(IOException ioException){
+                } catch (IOException ioException) {
                     ioException.printStackTrace();
                 }
             }
-    	});
+        });
     }
-    private class ClientThread extends Thread{
-    	private Socket socket = null;
+
+    private class ClientThread extends Thread {
+        private Socket socket = null;
 
         public ClientThread(Socket socket) {
-          super("ClientThread");
-          this.socket = socket;
+            super("ClientThread");
+            this.socket = socket;
         }
 
         public void run() {
-          try {
-        	  final ObjectOutputStream out = new ObjectOutputStream(this.socket.getOutputStream());
-              out.flush();
-              final ObjectInputStream in = new ObjectInputStream(this.socket.getInputStream());
-              System.out.println("server> "+(String)in.readObject());
-              final String ok = "{\"status\": 200, \"success\": true, \"becalm_key\":\""+REQUEST_SECRET_KEY+"\", \"data\": {} }";
-              out.writeObject(ok);
-		      out.flush();
-		      System.out.println("client> "+ok);
-              
-         } catch (Exception e) {
-          e.printStackTrace();
-         }
-       }
+            try {
+                final ObjectOutputStream out = new ObjectOutputStream(this.socket.getOutputStream());
+                out.flush();
+                final ObjectInputStream in = new ObjectInputStream(this.socket.getInputStream());
+                System.out.println("server> " + (String) in.readObject());
+                final String ok = "{\"status\": 200, \"success\": true, \"becalm_key\":\"" + REQUEST_SECRET_KEY + "\", \"data\": {} }";
+                out.writeObject(ok);
+                out.flush();
+                System.out.println("client> " + ok);
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
